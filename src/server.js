0113a4ba597b7431706fc7caa76ada9711a323f6
Clement Miao <clementmiao@gmail.com>
2015-10-25 11:23:30 -0700

    fix port issue

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import Server from 'socket.io';
 
 export default function startServer(store) {
-  const io = new Server().attach(8090);
+  const port = process.env.PORT || 8090;
+
+  const io = new Server().attach(port);
 
   store.subscribe(
     () => io.emit('state', store.getState().toJS())
